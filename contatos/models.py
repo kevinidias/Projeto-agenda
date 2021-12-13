ab81e4ea07d410bbe616952136e56fc0992ca39c
Kevin <kevin.dias.20160@gmail.com>
2021-12-13 19:45:22 -0300

    create models

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contato(models.Model):
+    nome = models.CharField(
+        max_length=50
+    )
+    sobrenome = models.CharField(
+        max_length=50,
+        blank=True
+    )
+    telefone = models.CharField(
+        max_length=50
+    )
+    email = models.CharField(
+        max_length=50,
+        blank=True
+    )
+    data_criacao = models.DateTimeField(
+        default=timezone.now
+    )
+    descricao = models.TextField(
+        blank=True
+    )
+    categoria_fk = models.ForeignKey(
+        'Categoria',
+        on_delete=models.DO_NOTHING
+
+    )
+
+class Categoria(models.Model):
+    nome = models.CharField(
+        max_length=50
+    )
+
+
+

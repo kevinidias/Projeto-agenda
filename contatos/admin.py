ac4fc7a2b5ba43446853c65ecd5f070621dbff06
Kevin <kevin.dias.20160@gmail.com>
2021-12-14 10:12:40 -0300

    config admin and add contacts

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Contato
+
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria_fk')
+    list_display_links = ('nome', 'sobrenome') # vira link
+    list_filter = ('data_criacao', 'nome')
+    list_per_page = 15
+
+
+admin.site.register(Contato, ContatoAdmin)
+
+
+admin.site.register(Categoria)
--- contatos/models.py
+++ contatos/models.py
@@ -30,10 +30,17 @@ class Contato(models.Model):
 
     )
 
+
+    def __str__(self):
+        return self.nome
+
 class Categoria(models.Model):
     nome = models.CharField(
         max_length=50
     )
+
+    def __str__(self):
+        return self.nome
 
 
 

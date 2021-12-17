030148ed60e76c6b505a5425bdb5fb670686fe40
Kevin <kevin.dias.20160@gmail.com>
2021-12-17 11:14:50 -0300

    add filter and orderby

--- contatos/views.py
+++ contatos/views.py
@@ -1,12 +1,15 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
 
 
 
 # pega os dados e retorna pro html
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 2)
 
     page = request.GET.get('p')
@@ -17,6 +20,10 @@ def index(request):
 
 def detalhes_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/detalhes_contato.html', { 
         'contato': contato }) 
 

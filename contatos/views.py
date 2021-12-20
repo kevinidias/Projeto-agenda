5282cf165347744c9880e253f3f294ee6cba8563
Kevin <kevin.dias.20160@gmail.com>
2021-12-20 11:21:07 -0300

    search field

--- contatos/views.py
+++ contatos/views.py
@@ -1,5 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 from django.http import Http404
 from .models import Contato
 
@@ -27,3 +29,23 @@ def detalhes_contato(request, contato_id):
     return render(request, 'contatos/detalhes_contato.html', { 
         'contato': contato }) 
 
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None or not termo:
+        raise Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome') #concatena os campos
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo) #filtra o nome completo ou por telefone
+    )
+
+    paginator = Paginator(contatos, 2)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', { 'contatos': contatos }) 

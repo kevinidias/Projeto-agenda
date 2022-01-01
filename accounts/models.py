3013c89154b4d5594f828b569cea6422f6dc6707
Kevin <kevin.dias.20160@gmail.com>
2022-01-01 18:38:34 -0300

    form dashboard

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django import forms
+from contatos.models import Contato
 
-# Create your models here.
+
+class FormContato(forms.ModelForm):
+    class Meta:
+        model = Contato
+        exclude = ()
